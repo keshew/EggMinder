4687ece64d7d614f66f835c238cd6314886d2302
keshew <artyom.korotckov2017@yandex.ru>
2025-03-20 17:07:10 +0500

    Initial Commit

---
+++ EggMinder/EggMinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggMinderApp.swift
+//  EggMinder
+//
+//  Created by Артём Коротков on 20.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct EggMinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
